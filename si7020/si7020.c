f116185aec08ae85e0fe4ba8e40a50ddc0d944f5
Marcin Benka <marcin.benka@gmail.com>
2017-06-26 00:15:27 +0200

    Update si7020 driver
    Improved error handling, fixed computation of temperature, added sanitycheck for calculated values.

--- si7020/si7020.c
+++ si7020/si7020.c
@@ -15,7 +15,7 @@
 
 #include "ehal/si7020/si7020.h"
 #include "ehal/i2c/i2c.h"
-#include "lib/i2c/i2c_march.h"
+#include "ehal/util/util.h"
 
 /***************************************************************************
  *	DEFINITIONS
@@ -23,6 +23,7 @@
 
 static i2c_cfg_st *i2c;
 
+// i2c command buffers
 static const char si7020_cmd_reset[] = {SI7020_CMD_RESET};
 static const char si7020_cmd_getfwver[] = {SI7020_CMD_FWVER1, SI7020_CMD_FWVER2};
 static const char si7020_cmd_getrh[] = {SI7020_CMD_GET_RH_NOHOLD};
@@ -57,15 +58,29 @@ BOOL si7020Init(const i2c_cfg_st *i2c_st)
 // --------------------------------------------------------------------------
 BOOL si7020GetMeasurement(SI7020_DATA_t *data)
 {
-	uint8_t s[2] = {0};
-	i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, si7020_cmd_getrh, 1, NULL, 0, SI7020_TIMEOUT);
-	delayMs(100);
-	i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, NULL, 0, &s, 2, SI7020_TIMEOUT);
-	data->humidity = (1250 * (s[1] | s[0] << 8))/65536 - 60;
+	uint8_t buf[2] = {0};
 
-	i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, si7020_cmd_gettemp, 1, NULL, 0, SI7020_TIMEOUT);
-	i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, NULL, 0, &s, 2, SI7020_TIMEOUT);
-	data->temperature = (1750.72f * (s[1] | s[0] << 8))/65536 - 468.5f;
+	// request humidity calculation
+	if (!i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, si7020_cmd_getrh, 1, NULL, 0, SI7020_TIMEOUT))
+		return (false);
+	// wait for conversion, it usualy takes about 22ms
+	delayMs(50);
+	// receive humidity
+	if (!i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, NULL, 0, (char*)&buf, 2, SI7020_TIMEOUT))
+		return (false);
+	data->humidity = (1250 * (buf[1] | buf[0] << 8))/65536 - 60;
+
+	// request temperature calculation from previous humidity calculation
+	if (!i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, si7020_cmd_gettemp, 1, NULL, 0, SI7020_TIMEOUT))
+		return (false);
+	// receive temperature
+	if (!i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, NULL, 0, (char*)&buf, 2, SI7020_TIMEOUT))
+		return (false);
+	data->temperature = (1757.2f * (buf[1] | buf[0] << 8))/65536 - 468.5f;
+
+	// sanitycheck
+	if (data->temperature > 1200 || data->temperature < -400)
+		return (false);
 
 	return (true);
 }
@@ -76,7 +91,7 @@ static UINT8 si7020_getFirmwareVersion(void)
 {
 	UINT8 fwver;
 
-	if (i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, si7020_cmd_getfwver, SI7020_CMD_FWVER_SIZE, (char*)&fwver, 1, SI7020_TIMEOUT))
+	if (i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, si7020_cmd_getfwver, sizeof(si7020_cmd_getfwver), (char*)&fwver, 1, SI7020_TIMEOUT))
 	{
 		if (SI7020_FWVER_REG_V1 == fwver || SI7020_FWVER_REG_V2 == fwver)
 			return (fwver);
@@ -87,7 +102,7 @@ static UINT8 si7020_getFirmwareVersion(void)
 // --------------------------------------------------------------------------
 static BOOL si7020_reset(void)
 {
-	return (i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, si7020_cmd_reset, SI7020_CMD_RESET_SIZE, NULL, 0, SI7020_TIMEOUT));
+	return (i2cMasterTransfer(i2c, true, SI7020_I2C_ADDR, si7020_cmd_reset, sizeof(si7020_cmd_reset), NULL, 0, SI7020_TIMEOUT));
 }
 
 // END
--- si7020/si7020.h
+++ si7020/si7020.h
@@ -27,24 +27,20 @@ extern "C" {
 
 // commands
 #define SI7020_CMD_RESET 0xFE
-#define SI7020_CMD_RESET_SIZE 1
 #define SI7020_CMD_FWVER1 0x84
 #define SI7020_CMD_FWVER2 0xB8
-#define SI7020_CMD_FWVER_SIZE 2
 #define SI7020_CMD_GET_RH_NOHOLD 0xF5
 #define SI7020_CMD_GET_PREV_TEMP 0xE0
-#define SI7020_CMD_RD_USR_REG 0xE6
 
 // register values
-#define SI7020_USR_REG_DEF 0b00111010
 #define SI7020_FWVER_REG_V1 0xFF
 #define SI7020_FWVER_REG_V2 0x20
 
 // data structure
 typedef struct
 {
-	UINT16 temperature;
-	UINT16 humidity;
+	INT16 temperature;
+	INT16 humidity;
 } SI7020_DATA_t;
 
 /***************************************************************************
